d1d1215ffd3e5eb93aa8ac5b88b66bea071adbb0
Mariana <marianabraz.souza01@gmail.com>
2023-01-13 16:58:58 -0300

    html css e js changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,10 @@ var iconMenu = document.querySelector('nav .menu-icon img');
 
 menuBar.addEventListener('click',function(){
 
-    if (iconMenu.getAttribute("src") == 'assets/imagens/menu.png') {
-        iconMenu.setAttribute("src","assets/imagens/close.png");
+    if (iconMenu.getAttribute("src") == 'imagens/menu.png') {
+        iconMenu.setAttribute("src","imagens/close.png");
     }else{
-        iconMenu.setAttribute("src","assets/imagens/menu.png");
+        iconMenu.setAttribute("src","imagens/menu.png");
     }
 
    menu.classList.toggle('active');
